5a6e528446816619bfca38925e35b1ce76384fb3
Miguel Sahelices Sarmiento <91277842+msahes00@users.noreply.github.com>
2025-04-16 17:53:22 +0000

    feat: Add colors to the main menu

--- frontend/src/views/MainMenu.tsx
+++ frontend/src/views/MainMenu.tsx
@@ -15,22 +15,38 @@ const styles = {
     display: flex;
     text-decoration: none;
   `,
+  red: css`
+    background-color: #990000;
+    color: #ffffff;
+  `,
+  blue: css`
+    background-color: #000099;
+    color: #ffffff;
+  `,
+  green: css`
+    background-color: #009900;
+    color: #ffffff;
+  `,
+  yellow: css`
+    background-color: #ff9900;
+    color: #ffffff;
+  `,
 };
 
 function MainMenu() {
   return (
     <>
       <div className={styles.container}>
-        <a className={styles.button} href="/shop">
+        <a className={[styles.button, styles.red].join(' ')} href="/shop">
           <p>Tienda</p>
         </a>
-        <a className={styles.button} href="/collection">
+        <a className={[styles.button, styles.blue].join(' ')} href="/collection">
           <p>Colleccion</p>
         </a>
-        <a className={styles.button} href="/draft">
+        <a className={[styles.button, styles.yellow].join(' ')} href="/draft">
           <p>Draft</p>
         </a>
-        <a className={styles.button} href="/fantasy">
+        <a className={[styles.button, styles.green].join(' ')} href="/fantasy">
           <p>Fantasy</p>
         </a>
       </div>
